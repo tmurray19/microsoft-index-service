e20c1a27a2e67c0be18144523c81235a710a9275
tmurray19 <me@taidghmurray.ie>
2019-10-10 14:14:17 +0100

    Access token as a function

--- video_status.py
+++ video_status.py
@@ -1,31 +1,42 @@
 ########### Python 3.2 #############
 import http.client, urllib.request, urllib.parse, urllib.error, base64
+import logging
+from config import Config
 
-headers = {
-    # Request headers
-    'x-ms-client-request-id': '',
-    'Ocp-Apim-Subscription-Key': '6b25b9e862fa4af4bab763ceaaf223cd',
-}
+def get_access_token(index_video_id):
+    # The id here MUST be the index video id, not the videosherpa ID 
+    logging.debug("This is for getting access token for a given private video private videos on account")
 
-params = {
-    # Request parameters
-    #'allowEdit': 'True',
-    'location': 'trial',   
-    'accountId': 'd1c32f1b-2a26-4b44-b6a3-c96e709d0648',
-    'videoId': '8e2e2ef6e4'
-}
+    headers = {
+        # Request headers
+        'x-ms-client-request-id': '',
+        'Ocp-Apim-Subscription-Key': Config.INDEX_SUBSCRIPTION_KEY,
+    }
 
-p_url = "/auth/{}/Accounts/{}/Videos/{}/AccessToken".format(params['location'], params['accountId'], params['videoId'])
+    params = {
+        # Request parameters
+        #'allowEdit': 'True',
+        'location': Config.INDEX_ACCOUNT_LOCATION,   
+        'accountId': Config.INDEX_ACCOUNT_ID,
+        'videoId': index_video_id
+    }
 
-params = urllib.parse.urlencode(params)
+    p_url = "/auth/{}/Accounts/{}/Videos/{}/AccessToken".format(params['location'], params['accountId'], params['videoId'])
 
-try:
-    conn = http.client.HTTPSConnection('api.videoindexer.ai')
-    conn.request("GET", p_url, headers=headers)
-    response = conn.getresponse()
-    data = response.read()
-    print(data)
-    conn.close()
-except Exception as e:
-    print(e)
+    params = urllib.parse.urlencode(params)
 
+    try:
+        logging.debug("Attempting to get access token for videos on account")
+        conn = http.client.HTTPSConnection('api.videoindexer.ai')
+        conn.request("GET", p_url, headers=headers)
+        response = conn.getresponse()
+        logging.debug("Access token for '{}' recieved".format(index_video_id))
+        data = response.read()
+        print(data[1:-1].decode("utf-8"))
+        conn.close()
+        return data[1:-1].decode("utf-8")
+    except:
+        logging.error("Error occured during access token of {}".format(index_video_id))
+        logging.exception("")
+        return None
+#get_access_token("cf40989adb")
